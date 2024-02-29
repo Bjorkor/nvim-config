40cfe00ee028d84d30147c08ff7e4207fb0c7629
Bjorkor <barkdawg11@gmail.com>
2024-02-28 20:59:49 -0600

    lsp ON

--- lua/bjorkor/lazy/lsp.lua
+++ lua/bjorkor/lazy/lsp.lua
@@ -1,17 +1,50 @@
 return {
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
     {'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         config = function()
             local lsp_zero = require("lsp-zero")
+
+            lsp_zero.extend_lspconfig()
             lsp_zero.on_attach(function(client, bufnr)
                 --keybinds go here
                 --:help lsp-zero-keybindings
                 lsp_zero.default_keymaps({buffer = bufnr})
             end)
+
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {"pyright"},
+                handlers = {
+                    lsp_zero.default_setup,
+                },
+            })
+            lsp_zero.setup_servers({'pyright', 'lua_ls', 'bashls'})
+            local cmp = require('cmp')
+            local cmp_action = lsp_zero.cmp_action()
+
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    -- `Enter` key to confirm completion
+                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+                    -- Ctrl+Space to trigger completion menu
+                    ['<C-Space>'] = cmp.mapping.complete(),
+
+                    -- Navigate between snippet placeholder
+                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+                    -- Scroll up and down in the completion documentation
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                })
+            })
         end
     },
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
 }
