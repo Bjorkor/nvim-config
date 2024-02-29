51d4d16783e2e5c04d60ca6bb33cc94583958421
Bjorkor <barkdawg11@gmail.com>
2024-02-28 19:17:21 -0600

    yes

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ install_nvim(){
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
     sudo rm -rf /opt/nvim
     sudo tar -C /opt -xzf nvim-linux64.tar.gz
+    sudo rm -rf nvim-linux64.tar.gz
     echo "adding nvim to path..."
     export PATH="$PATH:/opt/nvim-linux64/bin"
 }
