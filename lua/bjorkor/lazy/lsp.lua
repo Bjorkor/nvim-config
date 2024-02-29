fe2415b0a58dd44ef27714724e4d0faa9b8e312b
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2024-02-29 15:00:59 -0600

    YES

--- lua/bjorkor/lazy/lsp.lua
+++ lua/bjorkor/lazy/lsp.lua
@@ -24,27 +24,16 @@ return {
                     lsp_zero.default_setup,
                 },
             })
+            lsp_zero.omnifunc.setup({
+                autocomplete = true,
+                use_fallback = true,
+                update_on_delete = true,
+                trigger = '<C-Space>',
+            })
             lsp_zero.setup_servers({'pyright', 'lua_ls', 'bashls'})
             local cmp = require('cmp')
             local cmp_action = lsp_zero.cmp_action()
 
-            cmp.setup({
-                mapping = cmp.mapping.preset.insert({
-                    -- `Enter` key to confirm completion
-                    ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-                    -- Ctrl+Space to trigger completion menu
-                    ['<C-Space>'] = cmp.mapping.complete(),
-
-                    -- Navigate between snippet placeholder
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-                    -- Scroll up and down in the completion documentation
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                })
-            })
         end
     },
 }
