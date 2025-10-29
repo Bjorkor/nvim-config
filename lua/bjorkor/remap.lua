7e71978a433e8d8d0d2b1328a1fc812b6735b74c
Bjorkor <barkdawg11@Gmail.com>
2025-10-29 15:23:48 +0000

    yes

--- lua/bjorkor/remap.lua
+++ lua/bjorkor/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>c', '<Plug>OSCYankOperator')
+vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
+vim.keymap.set('v', '<leader>c', '<Plug>OSCYankVisual')
--- lua/bjorkor/set.lua
+++ lua/bjorkor/set.lua
@@ -11,4 +11,16 @@ vim.opt.scrolloff = 8
 vim.opt.termguicolors = true
 vim.opt.updatetime = 50
 vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+    name = 'lemonade',
+    copy = {
+        ['+'] = 'lemonade copy',
+        ['*'] = 'lemonade copy',
+    },
+    paste = {
+        ['+'] = 'lemonade paste',
+        ['*'] = 'lemonade paste',
+    },
+    cache_enabled = 1,
+}
 vim.api.nvim_set_var('python3_host_prog', '~/nvim-venv/bin/python')
